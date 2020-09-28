0abee643e34e449abd81810c7ae99103d53ec12c
esanolad <esanolad@gmail.com>
2020-09-28 15:17:28 +0100

    part 4 fix

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -14,8 +14,10 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl).pipe(map((response) => <Album>response.json()));
+      return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
+
+
 
 }
 /*
