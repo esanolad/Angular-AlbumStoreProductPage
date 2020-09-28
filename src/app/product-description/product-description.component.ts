a8cb98a51b7593e1da9e3d076beadfde698099c2
esanolad <esanolad@gmail.com>
2020-09-28 14:43:44 +0100

    implemets Part 4

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from "../product.service";
+import { Album } from "../album";
 
 @Component({
   selector: 'app-product-description',
@@ -8,18 +9,14 @@ import { ProductService } from "../product.service";
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  albumInfo
+  albumInfo: Album
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(
+      response => this.albumInfo = response
 
-    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
-    /*
-    this._productService.getAlbum(5).subscribe({
-      next: (response => this.albumInfo = response
-
-      )
-    }) */
+    )
   }
 
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,25 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from "@angular/http";
-import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
 import { map  } from 'rxjs/operators';
+import { Album } from "./album";
+import { Observable } from 'rxjs/Observable';
+import { jsonpCallbackContext } from '@angular/common/http/src/module';
 
-/*
 @Injectable()
 export class ProductService {
 
  private _albumUrl : string = "../assets/album.json"
 
-
   constructor(private _http: Http) { }
 
-
-  getAlbum(id: number): Observable<any> {
-    return this._http.get(this._albumUrl).pipe(map(response => response.json()))
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).pipe(map((response) => <Album>response.json()));
   }
 
-} */
-
+}
+/*
 @Injectable()
 export class ProductService {
 
@@ -31,4 +30,4 @@ export class ProductService {
     return this._http.get(this._albumUrl).pipe(map((response) => response.json()));
   }
 
-}
+} */
