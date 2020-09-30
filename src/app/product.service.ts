f507cd3e1104731bab0264431575e8f596e34cc2
esanolad <esanolad@gmail.com>
2020-09-30 08:37:39 +0100

    implements lesson 5

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -20,16 +20,3 @@ export class ProductService {
 
 
 }
-/*
-@Injectable()
-export class ProductService {
-
-  private _albumUrl = '../assets/album.json';
-
-  constructor(private _http: Http) { }
-
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).pipe(map((response) => response.json()));
-  }
-
-} */
