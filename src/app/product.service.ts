77e2ca220ea29f1f64f9f1494978a1abf8d72e66
esanolad <esanolad@gmail.com>
2020-09-30 09:03:57 +0100

    implements lesson6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,18 +4,25 @@ import 'rxjs/add/operator/map';
 import { map  } from 'rxjs/operators';
 import { Album } from "./album";
 import { Observable } from 'rxjs/Observable';
+import { Product } from "./product";
 import { jsonpCallbackContext } from '@angular/common/http/src/module';
 
 @Injectable()
 export class ProductService {
 
- private _albumUrl : string = "../assets/album.json"
+  private _productsUrl : string = "../assets/products.json"
+
+  private _albumUrl : string = "../assets/album.json"
 
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
       return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
+}
 
 
 
